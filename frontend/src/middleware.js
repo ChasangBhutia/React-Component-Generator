568d8e91a949dd4fb2801c0c7308d637dd949ba0
ChasangBhutia <chasangtsering@gmail.com>
2025-08-03 23:00:26 +0530

    fix: frontend middleware

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,16 +1,21 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
+  try {
+    const token = req.cookies.get("token");
+    console.log("Token in middleware:", token);
 
-  const token = req.cookies.get("token")?.value; 
+    if (req.nextUrl.pathname === "/" && !token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
 
-  if (req.nextUrl.pathname === "/" && !token) {
-    return NextResponse.redirect(new URL("/login", req.url));
+    return NextResponse.next();
+  } catch (err) {
+    console.error("Middleware error:", err);
+    return new NextResponse("Internal Error in Middleware", { status: 500 });
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/"], 
 };
