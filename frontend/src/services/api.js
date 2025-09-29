5ba4e869cec0ea0598d4139720f6140e8d356165
ChasangBhutia <chasangtsering@gmail.com>
2025-09-29 20:26:26 +0530

    fix: env for origin

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-    import.meta.env.MODE === "production"
-        ? import.meta.env.NEXT_PUBLIC_API_URL_PROD
-        : import.meta.env.NEXT_PUBLIC_API_URL_DEV;
+    process.env.MODE === "production"
+        ? process.env.NEXT_PUBLIC_API_URL_PROD
+        : process.env.NEXT_PUBLIC_API_URL_DEV;
 
 const api = axios.create({
     baseURL:`${BASE_URL}/api`,
